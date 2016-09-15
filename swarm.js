29f73a1c51d4a86141a7930bb5819b860d00ba03
sethflowers <seth.r.flowers@gmail.com>
2016-09-15 17:40:38 -0400

    strobing

--- swarm.js
+++ swarm.js
@@ -1,23 +1,37 @@
 class app {
-  constructor(context) {
+  constructor(canvas, context) {
+    this.canvas = canvas;
     this.context = context;
   }
 
-  run() {
-    this.context.fillStyle = 'green';
-    this.context.fillRect(0, 0, 700, 700);  
+  step() {
+    this.context.fillStyle = Math.random() > .5 ? 'white' : 'black';
+    this.context.fillRect(0, 0, this.width, this.height);  
+  }
+
+  setSize(width, height) {
+    this.width = width;
+    this.height = height;
+    this.canvas.width = width;
+    this.canvas.height = height;
   }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   let canvas = document.getElementById('screen');
-  canvas.width = document.body.clientWidth;
-  canvas.height = document.body.clientHeight;
-  
-  let runner = new app(canvas.getContext('2d'));
-  runner.run();
-});
+  let context = canvas.getContext('2d');  
+  let runner = new app(canvas, context);
 
-// window.addEventListener('resize', function(){
-//     alert('here');
-// });
+  runner.setSize(document.body.clientWidth, document.body.clientHeight);
+  
+  let pump = () => {
+    runner.step();
+    window.requestAnimationFrame(pump);
+  };
+
+  window.requestAnimationFrame(pump);
+
+  window.addEventListener('resize', () => {
+    runner.setSize(document.body.clientWidth, document.body.clientHeight);
+  });
+});
